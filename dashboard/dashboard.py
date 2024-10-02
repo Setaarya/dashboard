a7e3843d61c4950b39e1ec36aa197ae762351b23
unknown <ayatouchiha89@gmail.com>
2024-10-03 04:17:14 +0700

    Initial commit

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load cleaned datasets
-day_df = pd.read_csv("main_data.csv")
+day_df = pd.read_csv("D:/submission/dashboard/main_data.csv")
 
 # Convert 'dteday' to datetime for filtering
 day_df['dteday'] = pd.to_datetime(day_df['dteday'])
 
 # Menambahkan logo di sidebar
-logo_path = "pngaaa.com-5084798.png"  # Ganti dengan jalur logo Anda
+logo_path = "D:\submission\dashboard\pngaaa.com-5084798.png"  # Ganti dengan jalur logo Anda
 st.sidebar.image(logo_path, use_column_width=True)
 
 st.title("Dashboard Analisis Penyewaan Sepeda")
@@ -37,62 +38,61 @@ st.sidebar.markdown("**Sumber Data:** [Bike Sharing Dataset](https://www.kaggle.
 # Visualisasi 1: Perbandingan Sewa Sepeda Setiap Hari
 st.subheader("Perbandingan Penyewa Sepeda Setiap Hari")
 plt.figure(figsize=(10, 6))
-filtered_df.groupby('weekday')['cnt'].sum().reindex(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']).plot(kind='bar')
+sns.barplot(x='weekday', y='cnt', data=filtered_df, 
+            order=['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'], 
+            errorbar=None)
 plt.title('Perbandingan Penyewa Sepeda Setiap Hari')
 plt.xlabel('Hari')
 plt.ylabel('Jumlah Penyewa Sepeda')
-plt.xticks(rotation=0)  # Memutar label sumbu X horizontal
 st.pyplot(plt)
 
 # Visualisasi 2: Pengaruh Cuaca terhadap Penyewaan Sepeda
 st.subheader("Pengaruh Cuaca terhadap Penyewaan Sepeda")
 plt.figure(figsize=(10, 6))
-filtered_df.groupby('weathersit')['cnt'].sum().plot(kind='bar')
+sns.barplot(x='weathersit', y='cnt', data=filtered_df, errorbar=None)
 plt.title('Jumlah Penyewa Sepeda berdasarkan Kondisi Cuaca')
 plt.xlabel('Kondisi Cuaca')
 plt.ylabel('Jumlah Penyewa Sepeda')
-plt.xticks(rotation=0)  # Memutar label sumbu X horizontal
 st.pyplot(plt)
 
 # Visualisasi 3: Sewa Sepeda Berdasarkan Jenis Hari
 st.subheader("Sewa Sepeda Berdasarkan Jenis Hari")
 plt.figure(figsize=(10, 6))
-filtered_df.groupby(['mnth', 'workingday'])['cnt'].sum().unstack().plot(kind='line', marker='o')
+sns.pointplot(data=filtered_df, x='mnth', y='cnt', hue='workingday', 
+              order=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 
+                     'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], errorbar=None)
 plt.title('Jumlah Penyewa Sepeda berdasarkan Jenis Hari')
 plt.xlabel('Bulan')
 plt.ylabel('Jumlah Penyewa Sepeda')
-plt.xticks(ticks=range(len(filtered_df['mnth'].unique())), labels=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], rotation=0)  # Memutar label sumbu X horizontal
 st.pyplot(plt)
 
 # Visualisasi 4: Sewa Sepeda Berdasarkan Musim
 st.subheader("Sewa Sepeda Berdasarkan Musim")
 plt.figure(figsize=(10, 6))
-filtered_df.groupby('season')['cnt'].sum().plot(kind='bar')
+sns.barplot(x='season', y='cnt', data=filtered_df, errorbar=None)
 plt.title('Jumlah Penyewa Sepeda berdasarkan Musim')
 plt.xlabel('Musim')
 plt.ylabel('Jumlah Penyewa Sepeda')
-plt.xticks(rotation=0)  # Memutar label sumbu X horizontal
 st.pyplot(plt)
 
 # Visualisasi 5: Jumlah Penyewa Sepeda Tiap Bulan
 st.subheader("Jumlah Penyewa Sepeda Tiap Bulan")
 plt.figure(figsize=(10, 6))
-filtered_df.groupby('mnth')['cnt'].sum().plot(kind='bar')
+sns.barplot(data=filtered_df, x='mnth', y='cnt', 
+            order=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 
+                   'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], errorbar=None)
 plt.title('Penyewa Sepeda Setiap Bulan')
 plt.xlabel('Bulan')
 plt.ylabel('Jumlah Penyewa Sepeda')
-plt.xticks(ticks=range(len(filtered_df['mnth'].unique())), labels=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], rotation=0)  # Memutar label sumbu X horizontal
 st.pyplot(plt)
 
 # Visualisasi 6: Tren Penyewaan Sepeda Tiap Bulan
 st.subheader("Tren Penyewaan Sepeda Tiap Bulan")
 plt.figure(figsize=(10, 6))
-filtered_df.groupby(['mnth', 'yr'])['cnt'].sum().unstack().plot(kind='line', marker='o')
-plt.title('Tren Penyewaan Sepeda')
+sns.pointplot(data=filtered_df, x='mnth', y='cnt', hue='yr', 
+              order=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 
+                     'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], errorbar=None)
+plt.title('Tren Penyewa Sepeda')
 plt.xlabel('Bulan')
 plt.ylabel('Jumlah Penyewa Sepeda')
-<<<<<<< HEAD
-plt.xticks(ticks=range(len(filtered_df['mnth'].unique())), labels=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], rotation=0)  # Memutar label sumbu X horizontal
-=======
->>>>>>> 12d64d39fecab8283b5c328574fcbb9f155b62a0
 st.pyplot(plt)
