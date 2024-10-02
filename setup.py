a598d89bf210539ae0d4c7765380579ef0ca1fb7
Setaarya <146357135+Setaarya@users.noreply.github.com>
2024-10-03 02:27:06 +0700

    first commit

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
-# .streamlit/secrets.toml
-[python]
-version = "3.9"
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='3.9',
+    author='Yulianto Aryaseta',
+    author_email='yuliantoseta@gmail.com',
+    description='Dashboard',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',  # Daftar ketergantungan
+        'pandas',
+    ],
+)
